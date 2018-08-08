3f8c80838e142fbdf2f618268af338bcb0c009d4
Hayden <33820650+sirredbeard@users.noreply.github.com>
2018-08-08 14:25:08 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,23 +16,17 @@ sudo apt-get install aria2 youtube-dl synaptic -y
 
 sudo add-apt-repository ppa:mozillateam/firefox-next -y
 
-sudo apt-get update
-
 sudo apt-get install firefox -y
 
 # install java 8
 
 sudo add-apt-repository ppa:webupd8team/java -y
 
-sudo apt-get update
-
 sudo apt-get install oracle-java8-installer -y
 
 # install libreoffice 6
 
 sudo add-apt-repository ppa:libreoffice/libreoffice-prereleases -y
-
-sudo apt-get update
 
 sudo apt-get install libreoffice -y
 
